1ae195498f254ca85fc5b12190e4c88c8bc2cd11
DESKTOP-IV23AFT\Gilles Muganza <gillesmg13@gmail.com>
2022-01-09 19:12:11 +0100

    handling cart localStarge and children technical props in Banner

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,35 @@ import Cart from './Cart';
 import Footer from './Footer';
 import ShoppingList from './ShoppingList';
 
-import "../styles/Layout.css"
-import { useState } from 'react';
+import logo from '../assets/logo.png'
+
+import '../styles/Layout.css'
+import '../styles/Banner.css'
+
+import { useEffect, useState } from 'react';
 
 function App() {
+ 
+  /* -- Gestion sauvegarde et lecture du cart via le localStorage ----------- */
+  const savedCart = localStorage.getItem('cart')
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+  /* ----------------------------------------------------------------------- */
 
-  const [cart, updateCart] = useState([])
+  /* -- Comportement sans localStorage ------------------------------------- * /
+  const savedCart = undefined
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+  /* ----------------------------------------------------------------------- */
+
   const [activeCategorie, setActiveCategorie] = useState('')
 
   return (<>
-          <Banner />
+          <Banner className='lmj-banner-row'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>La Maison Jungle</h1>
+          </Banner>
           <div className='lmj-layout-inner'>
             <Cart
               cart={cart}
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,10 @@ import '../styles/Banner.css'
 import logo from '../assets/logo.png'
 import Recommendation from './Recommendation'
 
-
+/*
 const Banner = () => (
     <>
-    <div className='lmj-banner'>
+    <div className='lmj-banner-initial'>
         <div className='lmj-banner-row'>
             <img src={logo} alt='La maison jungle' className='lmj-logo' />
             <h1 className='lmj-title'>La Maison Jungle</h1>
@@ -15,16 +15,14 @@ const Banner = () => (
     
     </>
 )
-
-/* 
-Autre style sd'ecriture d'un composant:
-
-function Banner() {
+*/
+/* - Autre style d'ecriture avec la props technique children: -------------- */
+function Banner({ children }) {
     return (
-        <h1>La Maison Jungle</h1>
+        <div className='lmj-banner'>{children}</div>
     )
 }
-*/
+/* ------------------------------------------------------------------------- */
 
 
 export default Banner
